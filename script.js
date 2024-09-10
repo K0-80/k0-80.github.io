b28ebbc9a7757f04a402654fe3df4eee11b79dcb
K0 <K080629@outlook.com>
2024-09-09 20:30:56 -0400

    new
    dawda

--- script.js
+++
@@ -1,7 +0,0 @@
-// Add event listener to paint container
-document.addEventListener("DOMContentLoaded", () => {
-    const paintContainer = document.querySelector(".paint-container");
-    paintContainer.addEventListener("animationend", () => {
-      paintContainer.classList.add("hidden");
-    });
-  });
